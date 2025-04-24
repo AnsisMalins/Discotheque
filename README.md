0f3b95d1d5d6fdfeaa4721cc7682e11a60ca2ecc
Ansis <ansis.malins@decentraland.org>
2025-04-24 23:22:42 +0200

    React to direction inputs

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discotheque
+
+A crude replica of a game like [StepMania](https://www.stepmania.com/) or [Friday Night Funkin](https://www.newgrounds.com/portal/view/770371) as a [Decentraland](https://decentraland.org/) scene.
---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { Color4 } from '@dcl/sdk/math'
+import { engine, Entity, InputAction, Material, pointerEventsSystem } from '@dcl/sdk/ecs'
+
+export function main() {
+    const arrowNames = ["Left", "Down", "Up", "Right"] as const;
+    type ArrowName = typeof arrowNames[number];
+    const arrowTexture = Material.Texture.Common({ src: "assets/scene/Images/arrow.png" })
+
+    const upMaterial = {
+        texture: arrowTexture,
+        albedoColor: Color4.White()
+    }
+
+    const downMaterial = {
+        texture: arrowTexture,
+        albedoColor: Color4.Red()
+    }
+
+    let arrows: Record<ArrowName, Entity>
+
+    {
+        const arrowsPartial: Partial<typeof arrows> = { }
+
+        for (let i = 0; i < arrowNames.length; i++) {
+            let entity = engine.getEntityOrNullByName(arrowNames[i])
+    
+            if (entity == null)
+                throw new Error(`${arrowNames[i]} is null`)
+
+            arrowsPartial[arrowNames[i]] = entity;
+        }
+
+        arrows = arrowsPartial as typeof arrows
+    }
+
+    const hitBox = engine.getEntityOrNullByName("HitBox")
+
+    if (hitBox == null)
+        throw new Error("HitBox is null")
+
+    const pointerData = {
+        entity: hitBox,
+        opts: {
+            hoverText: ""
+        }
+    }
+
+    pointerEventsSystem.onPointerDown(pointerData, cmd => {
+        switch (cmd.button) {
+            case InputAction.IA_LEFT:
+                Material.setPbrMaterial(arrows.Left, downMaterial)
+                break
+            case InputAction.IA_BACKWARD:
+                Material.setPbrMaterial(arrows.Down, downMaterial)
+                break
+            case InputAction.IA_FORWARD:
+                Material.setPbrMaterial(arrows.Up, downMaterial)
+                break
+            case InputAction.IA_RIGHT:
+                Material.setPbrMaterial(arrows.Right, downMaterial)
+                break
+        }
+    })
+
+    pointerEventsSystem.onPointerUp(pointerData, cmd => {
+        switch (cmd.button) {
+            case InputAction.IA_LEFT:
+                Material.setPbrMaterial(arrows.Left, upMaterial)
+                break
+            case InputAction.IA_BACKWARD:
+                Material.setPbrMaterial(arrows.Down, upMaterial)
+                break
+            case InputAction.IA_FORWARD:
+                Material.setPbrMaterial(arrows.Up, upMaterial)
+                break
+            case InputAction.IA_RIGHT:
+                Material.setPbrMaterial(arrows.Right, upMaterial)
+                break
+        }
+    })
+}
